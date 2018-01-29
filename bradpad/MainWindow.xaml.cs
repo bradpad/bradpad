45e7ffe28bc16f6cb5de57a9eeede95b0ab5b0fe
Ivor Huang <ivorh97@gmail.com>
2018-01-29 14:39:51 -0500

    wpfapp2 -> bradpad

---
+++ bradpad/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bradpad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void settingsButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.mainCanvas.Visibility = System.Windows.Visibility.Visible;
+            this.settingsCanvas.Visibility = System.Windows.Visibility.Hidden;
+            //count++;
+            //this.settingsButton.Content = count;
+        }
+
+        public void mainButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.mainCanvas.Visibility = System.Windows.Visibility.Hidden;
+            this.settingsCanvas.Visibility = System.Windows.Visibility.Visible;
+            //count2++;
+            //this.mainButton.Content = count2;
+        }
+    }
+}
