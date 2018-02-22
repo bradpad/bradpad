f950204bbd89303cacdbd143ccc4711a05672ba5
Ivor Huang <ivorh97@gmail.com>
2018-02-22 00:20:29 -0500

    move tutorial text to string variables

--- bradpad/MainWindow.xaml.cs
+++ bradpad/MainWindow.xaml.cs
@@ -20,28 +20,30 @@ namespace bradpad {
     public partial class MainWindow : Window {
 
         //Dictionary<string, string> applicationInfo = new Dictionary<string, string>();
-        string recentName;
+        //string recentName;
         private App app = ((App)Application.Current);
 
-        public MainWindow() {
-            InitializeComponent();
-            foreGroundCheckBox.IsChecked = Topmost;
-            recentName = "";
-            MessageBox.Show(
-                "Welcome to the alpha release of bradpad! Right now, this product is designed to do copy and paste instructions and open a desired application. The buttons on the Arduino hardware expedite this procedure. A step-by-step tutorial is below. In the upcoming releases, we hope to be able to have more functionality, as well as being able to add custom commands. We hope you enjoy!\n\n" +
-                "1. Pressing the left pedal will open Microsoft Word.\n" +
-                "2. Pressing the middle pedal corresponds to a copy (CTRL + C shortcut) command.\n" +
-                "3. Pressing the right pedal corresponds to a paste (CTRL + V shortcut) command.\n\n" +
-                "If you get lost, you can press the Help button in the Settings view to view these instructions again.\n" +
-                "The application also has the capability to customize the application that is mapped to the left pedal, but the UI is a undeveloped because this flow will be changed in the future.\n\n" +
+        private string tutorialCaption = "bradpad Help Screen and Tutorial";
+        private string tutorialText =
+                "Welcome to the alpha release of bradpad! Right now, this product is designed to be able to open Microsoft Word and execute copy and paste instructions. The buttons on the Arduino hardware act as pedals. A step-by-step tutorial is below. In the upcoming releases, we hope to increase functionality and allow users to add custom commands. We hope you enjoy!\n\n" +
+                "1. Plug the Arduino and pedal system into the computer.  The computer will detect them as a keyboard.\n" +
+                "2. Pressing the left pedal will open Microsoft Word.\n" +
+                "3. Pressing the middle pedal corresponds to a copy (CTRL + C shortcut) command.\n" +
+                "4. Pressing the right pedal corresponds to a paste (CTRL + V shortcut) command.\n\n" +
+                "If you get lost, you can press the Help button in the Settings view to view these instructions again.\n\n" +
+                "The application also has the capability to customize the application that is mapped to the left pedal, but the UI is a undeveloped because this flow will be changed in the future.  This is a stretch goal implemented from our beta release goals.\n\n" +
                 "Opening an Application\n" +
                 "1. In the settings view, navigate to the applications view.\n" +
                 "2. Input a name for the application in the \"Enter Application Name\" field.\n" +
                 "3. Enter the application location. This can be found by opening the application location through file explorer, and right clicking the address. Copy this and paste it into the application where it says \"Enter Application Location\".\n" +
                 "4. Click the \"Enter\" button. This triggers the left pedal (or clicking the left panel on the main screen) to open the desired application.\n" +
-                "5. The process can be repeated to change the desired application.",
-                "bradpad Help Screen and Tutorial"
-            );
+                "5. The process can be repeated to change the desired application.";
+
+        public MainWindow() {
+            InitializeComponent();
+            foreGroundCheckBox.IsChecked = Topmost;
+            //recentName = "";
+            MessageBox.Show(tutorialText, tutorialCaption);
         }
 
         // Main Panel
@@ -79,8 +81,9 @@ namespace bradpad {
 
         private void HelpButtonClicked(object sender, RoutedEventArgs e)
         {
-            settingsPanel.Visibility = Visibility.Hidden;
-            helpPanel.Visibility = Visibility.Visible;
+            //settingsPanel.Visibility = Visibility.Hidden;
+            //helpPanel.Visibility = Visibility.Visible;
+            MessageBox.Show(tutorialText, tutorialCaption);
         }
         
         private void ForegroundCheckBoxClicked(object sender, RoutedEventArgs e) {
@@ -99,7 +102,7 @@ namespace bradpad {
             app.SetMapping(App.F22, tt, true);
             F22.Content = t;
             //applicationInfo[t] = tt;
-            recentName = t;
+            //recentName = t;
             addAppLocation.GotFocus += TextBox_GotFocus;
             addAppText.GotFocus += TextBox_GotFocus;
             currentName.Text = "Current Application Name: " + t;
